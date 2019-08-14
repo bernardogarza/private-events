98e429a97871c39c244839e9f5a3809a11411305
Kalib <nagui08@hotmail.com>
2019-08-14 13:12:42 -0500

    sessions controller done, working on the views part

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,37 @@
 module SessionsHelper
+    def log_in(user)
+        current_user = user
+    end
+    
+    def current_user
+        user = if session[:user_id]
+                 User.find(sessions[:user_id])
+               elsif cookies[:user_id]
+                 User.find(cookies[:user_id])
+               end
+    end
+        
+    def logged_in?
+        !!current_user
+    end
+    
+    def current_user=(user)
+        sessions[:user_id] = user.id
+        current_user = current_user
+    end
+
+    def sign_out(user)
+        session.delete(:user_id) if user == current_user
+    end
+
+    def remember(user)
+		user.remember
+		cookies.permanent[:user_id] = user.id
+		cookies.permanent[:remember_digest] = user.remember_digest
+	end
+
+	def forget(_user)
+		cookies.delete(:user_id)
+		cookies.delete(:remember_digest)
+	end
 end
