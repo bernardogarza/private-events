ddf2cd3dc418615a016268b062133a0f87e80b8f
bernardogarza <bernardo.g.landa@gmail.com>
2019-08-15 11:40:45 -0500

    created the an atendee model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ApplicationRecord
     validates :title, :description, :location, :date, presence: true
 
     belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
+    has_many :attendee, through: :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_202628) do
+ActiveRecord::Schema.define(version: 2019_08_15_161927) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.integer "attendee"
+    t.integer "attended_event"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
