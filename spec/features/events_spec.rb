0498d87445f2ac58b0bf1c0e4f29959a3e52c495
bernardogarza <bernardo.g.landa@gmail.com>
2019-08-26 13:43:23 -0500

    Working on the rspec tests

---
+++ spec/features/events_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.feature "Events", type: :feature do
+  context 'Testing events controller' do
+    
+  end
+end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,6 +2,23 @@ require 'rails_helper'
 
 RSpec.feature "Users", type: :feature do
   context 'Testing Users Controller' do
-    
+    scenario 'Should be success' do
+      visit users_new_path
+      within('form') do
+        fill_in 'Username:', with: 'foo'
+        fill_in 'Email:', with: 'foo@bar.com'
+      end
+      click_button 'Sign Up'
+      expect(page).to have_content('Log Out')
+    end
+    scenario 'Should fail' do
+      visit users_new_path
+      within('form') do
+        fill_in 'Username:', with: 'foo'
+        fill_in 'Email:', with: 'foo@bar'
+      end
+      click_button 'Sign Up'
+      expect(page).to have_content('Email is invalid')
+    end
   end
 end
